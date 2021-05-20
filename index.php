c0ecb1d5d8089d6d0de15487694e1510bf472494
Mohamed Ibrahiem Elfert <me_me4152@yahoo.com>
2021-05-20 23:09:49 +0400

    new change
    just length for videos title that showed in home page
    
    Co-Authored-By: Mohamed Ibrahiem Elfert <me_me4152@yahoo.com>

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
                       </div>
                       <div style="padding: 5px;background: #f6f5f7;border: solid 1px #e7e7e7;margin-bottom: 20px;">
                         <div style="max-height: 190px;min-height: 190px;"><img src="libs/upload/<?php echo $value['image']; ?>" class="img-responsive" style="width: 100%;min-height: 20px;max-height: 190px;" /></div>
-                        <h5 style="background: #fff;padding: 5px;text-align: center;margin: 5px 0;font-weight: bold;border: solid 1px #e2e0e4;"><?php echo (mb_strlen($value['title'] , 'utf8') > 40 ? mb_substr($value['title'],0,40) . ' ...' : $value['title']); ?></h5>
+                        <h5 style="background: #fff;padding: 5px;text-align: center;margin: 5px 0;font-weight: bold;border: solid 1px #e2e0e4;"><?php echo (mb_strlen($value['title'] , 'utf8') > 35 ? mb_substr($value['title'],0,35) . ' ...' : $value['title']); ?></h5>
                         <a href="category.php?cat=<?php echo $category->getCatNameUniqueById($value['category']); ?>"><h5 style="background: #607D8B;padding: 5px 12px;text-align: center;margin: 5px 0;border: solid 1px #546f7b;position: absolute;top: 30px;color: #fff;"><?php echo $category->getCatNameById($value['category']); ?></h5></a>
                         <p style="background: #fff;padding: 5px;text-align: justify;margin: 5px 0;border: solid 1px #e2e0e4;min-height: 100px;max-height: 100px;"><?php echo (mb_strlen($value['description'],'utf8') > 120 ? mb_substr($value['description'],0,120) . ' ...' : $value['description']); ?></p>
                         <div class="text-center"><a href="video.php?v=<?php echo $value['videoLink']; ?>" class="btn btn-danger">شاهد الآن</a></div>
